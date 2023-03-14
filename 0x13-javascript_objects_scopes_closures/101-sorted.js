e080db0e975d8a37ed9e2613bc512c1385af25a5
Sphamandla-code <nkambulesphamandla@gmail.com>
2023-03-14 15:17:53 +0200

    0x13

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-// function that returns the number of occurrences in a list
 
-const originalList = require('./101-data').dict;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-  return (e * index);
-});
-console.log(mappedList);
+const dict = require('./101-data').dict;
+const output = {};
+
+for (const entry in dict) {
+  if (!output[dict[entry]]) output[dict[entry]] = [];
+  output[dict[entry]].push(entry);
+}
+
+console.log(output);

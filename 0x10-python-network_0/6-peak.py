dcd299494aad4b59e3d7b497250044697114f350
Sphamandla-code <nkambulesphamandla@gmail.com>
2023-03-30 16:45:38 +0200

    0x10

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+"""my fun"""
 
 def find_peak(list_of_integers):
-
+    """find peak """
+    
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
 

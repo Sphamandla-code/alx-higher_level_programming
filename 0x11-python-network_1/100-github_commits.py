b0bb6b87f253210fe087ffb32b700d919c7bd174
Sphamandla-code <nkambulesphamandla@gmail.com>
2023-03-31 15:00:02 +0200

    0x11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 """
-list 10 commits (from the most recent to oldest) of the repository “rails”
-by the user “rails”
+Python script that shows the last 10 commits of a repository
+in GitHub
 """
-import requests
-from sys import argv
+from requests import get, auth
+import sys
 
 
 if __name__ == "__main__":
-    """
-    list 10 commits (from the most recent to oldest) of the repository
-    “rails” by the user “rails”
-    """
-    repo = argv[1]
-    owner = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    r = requests.get(url)
-    res_list = r.json()
     try:
-        for i in range(10):
-            print("{}: {}".format(res_list[i].get('sha'), res_list[i].
-                                  get('commit').get('author').get('name')))
+        repo = sys.argv[1]
+        owner = sys.argv[2]
+        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+        r = get(url)
+        json_o = r.json()
+        for i in range(0, 10):
+            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
+                                  .get('author').get('name')))
     except:
         pass

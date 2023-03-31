0ae2f8782a3b596e5c889a8e317c700e11d1352d
Sphamandla-code <nkambulesphamandla@gmail.com>
2023-03-31 12:06:08 +0200

    0x10

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a POST request to the passed URL with data
-curl -sX POST --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sX POST --data "email=test@gmail.com&subject=I will always be here for PLD" "$1"

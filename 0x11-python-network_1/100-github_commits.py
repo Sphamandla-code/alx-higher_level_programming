c3736925f3bf36d01f20ef32d7f38b8decb5f94e
Sphamandla-code <nkambulesphamandla@gmail.com>
2023-03-31 14:57:55 +0200

    0x11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,20 +4,22 @@ list 10 commits (from the most recent to oldest) of the repository “rails”
 by the user “rails”
 """
 import requests
-import sys
+from sys import argv
 
-def main():
-    """
-    main function
-    """
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        print(response.json())
-    else:
-        print(f"Error: {response.status_code}")
 
 if __name__ == "__main__":
-    main()
+    """
+    list 10 commits (from the most recent to oldest) of the repository
+    “rails” by the user “rails”
+    """
+    repo = argv[1]
+    owner = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url)
+    res_list = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(res_list[i].get('sha'), res_list[i].
+                                  get('commit').get('author').get('name')))
+    except:
+        pass

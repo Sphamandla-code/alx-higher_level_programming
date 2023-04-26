9a4dfe3db564a04dd51d3a95ad5bb513ae8d15f0
Sphamandla-code <nkambulesphamandla@gmail.com>
2023-04-26 10:34:19 +0200

    0x14

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// prints title of a Star Wars movie where episode number matches a given integer.
+
+const req = require('request');
+const episode = process.argv[2];
+const url = 'http://swapi.co/api/films/' + episode;
+
+req(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
